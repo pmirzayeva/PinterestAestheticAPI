41c36abd5a963b2c04702d8d76f6c0d4cee59985
pmirzayeva <pmirzayeva1@gmail.com>
2024-01-08 20:14:24 +0100

    updated

--- assets/index.js
+++ assets/index.js
@@ -6,7 +6,7 @@ async function fetchAndDisplayImages(query = 'random') {
         const url = `https://api.unsplash.com/search/photos?query=${query}&client_id=${apiKey}&per_page=${limit}`;
         const response = await fetch(url);
         const { results } = await response.json();
-        // console.log({results});
+        console.log({results});
 
         const box = document.querySelector(".box");
         box.innerHTML = results.map(photo => `<img src="${photo.urls.full}" alt="${photo.alt_description}">`).join('');
